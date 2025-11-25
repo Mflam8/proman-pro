f5c3a8286aba02656a90bf2536e01d8186e15c16
Marion Flamenco <mayaflamenco89@gmail.com>
2025-11-25 22:22:09 +0000

    File changes

--- functions/generateInvoice.ts
+++ functions/generateInvoice.ts
@@ -280,8 +280,8 @@ Deno.serve(async (req) => {
         
         const { file_url } = await base44.asServiceRole.integrations.Core.UploadFile({ file });
 
-        // Actualizar el inquiry (usar el id original del raw)
-        await base44.asServiceRole.entities.ClientInquiry.update(inquiryRaw.id || inquiryId, {
+        // Actualizar el inquiry
+        await base44.asServiceRole.entities.ClientInquiry.update(inquiryId, {
             quote_pdf_url: file_url
         });
 
